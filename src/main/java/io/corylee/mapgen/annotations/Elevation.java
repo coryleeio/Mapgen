aea935d828bb52d0f1ec92f6486b2c3631e68080
Cory Lee <cl1001123@gmail.com>
2014-12-15 23:42:41 -0500

    Add enums, packages, and initial version

---
+++ src/main/java/io/corylee/mapgen/annotations/Elevation.java
@@ -0,0 +1,9 @@
+package io.corylee.mapgen.annotations;
+
+public enum Elevation {
+	// Do not reorder without migration
+	FLAT,
+	ROCKY,
+	HILLY,
+	MOUNTAINOUS
+}
---
+++ src/main/java/io/corylee/mapgen/annotations/Moisture.java
@@ -0,0 +1,11 @@
+package io.corylee.mapgen.annotations;
+
+public enum Moisture {
+	// Do not reorder without migration
+	WET_1,
+	WET_2,
+	WET_3,
+	WET_4,
+	WET_5,
+	WET_6
+}
