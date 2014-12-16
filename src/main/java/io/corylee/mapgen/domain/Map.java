91b6f9f5f020e1afefd9c6a78f1ed12f027049ba
Cory Lee <cl1001123@gmail.com>
2014-12-16 02:54:55 -0500

    clean

--- src/main/java/io/corylee/mapgen/domain/Map.java
+++ src/main/java/io/corylee/mapgen/domain/Map.java
@@ -1,16 +1,16 @@
 package io.corylee.mapgen.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * A Map.
